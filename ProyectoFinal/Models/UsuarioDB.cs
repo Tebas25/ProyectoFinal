3a80bb04b55179b0d8bce82babf2cb71b066770d
Esteban Alejandro Narváez Acurio <esteban.narvaez.acurio@udla.edu.ec>
2025-01-26 23:36:00 -0500

    Creacion de base de datos Sqlite

---
+++ ProyectoFinal/Models/UsuarioDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+
+namespace ProyectoFinal.Models
+{
+    [Table("Usuarios")]
+    public class UsuarioDB
+    {
+        [PrimaryKey, AutoIncrement]
+        public int UsuarioId { get; set; }
+        public string Correo { get; set; }
+        public string Username { get; set; }
+        public string Clave { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Membresia { get; set; }
+        public string claveAdministrador { get; set; }
+    }
+}
---
+++ ProyectoFinal/Services/BaseUsuarios.cs
@@ -0,0 +1,33 @@
+﻿using ProyectoFinal.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Services
+{
+
+    public class BaseUsuarios
+    {
+        readonly SQLiteAsyncConnection baseUsuarios;
+
+        public BaseUsuarios(string path)
+        {
+            baseUsuarios = new SQLiteAsyncConnection(path);
+            baseUsuarios.CreateTableAsync<UsuarioDB>().Wait();
+        }
+
+        public Task<List<UsuarioDB>> GetAll()
+        {
+            return baseUsuarios.Table<UsuarioDB>().ToListAsync();
+        }
+
+        public Task<int> Guardar(UsuarioDB usuario)
+        {
+            return baseUsuarios.InsertAsync(usuario);
+        }
+
+    }
+}
